5882855c97daca7b9aa712c51d819d1cfd33645f
haeramee <haeramee@naver.com>
2021-06-26 22:04:52 +0900

    third commit

--- findingRoomie/findingRoomie/urls.py
+++ findingRoomie/findingRoomie/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from user.views import write
 from django.contrib import admin
 from django.urls import path
+from user.views import view, write
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', write, name="write")
+    path('view', view, name="view"),
+    path('write', write, name="write")
 ]
--- findingRoomie/user/views.py
+++ findingRoomie/user/views.py
@@ -2,6 +2,29 @@ from django.shortcuts import render
 from .models import Userinfo
 
 # Create your views here.
-def write(request):
+def view(request):
     userdic = Userinfo.objects.all()
-    return render(request, 'write.html', {'userdic': userdic})
+    return render(request, 'view.html', {'userdic': userdic})
+
+def write(request):
+    return render(request, 'write.html')
+
+def create(request):
+    new_profile = Userinfo
+    request.POST['realname']
+    request.POST['nickname']
+    request.POST['gender_m']
+    request.POST['gender_f']
+    request.POST['age']
+    request.POST['major']
+    request.POST['studentID']
+    request.POST['introduction']
+    request.POST['profile_img']
+    request.POST['sleep_time']
+    request.POST['cleanliness']
+    request.POST['cook']
+    request.POST['smoker']
+    request.POST['nonsmoker']
+    request.POST['rent']
+    request.POST['deposit']
+    request.POST['hope_area']
